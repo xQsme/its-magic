eaf61e21e74d977c29681e11d75255e7e497bcba
yvtq8k3n <yvtq8k3n@gmail.com>
2020-10-09 11:09:38 +0100

    Returning roles with Token, Recriating DB, Adding groups to admin

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
+import json 
 from datetime import datetime
 from calendar import timegm
 #from rest_framework_jwt.settings import api_settings
 
 def jwt_response_payload_handler(token, user=None, request=None):
+
+    l = user.groups.values_list('name',flat = True) # QuerySet Object
+    user_roles = list(l)
     """ 
     Custom response payload handler.
     This function controlls the custom payload after login or token refresh. This data is returned through the web API.
@@ -13,6 +17,6 @@ def jwt_response_payload_handler(token, user=None, request=None):
             'id':user.id,
             'username':user.username,
 			'email': user.email,
-            'role': "admin" if user.is_staff else "preb"
+            'role': user_roles
         }
     }
