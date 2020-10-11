f6a9996710f4b23ab667977a6f1ca73b21897f8b
SWarrior97 <carlosgomes1997.edu@gmail.com>
2020-10-11 13:56:58 +0100

    register html done. TODO send data to server

--- svelte/src/components/Register.svelte
+++ svelte/src/components/Register.svelte
@@ -1,13 +1,33 @@
+<script lang="ts">
+    let firstName:string[];
+    let lastName:string[];
+    let username:string[];
+    let email:string[];
+    let password:string[];
+    let confirmPassword:string[];
+
+    function createUser() {
+		if(password != confirmPassword){
+            alert("Password and confirm password doesnt match")
+        }else{
+            //TODO
+            //register
+        }
+	}
+    
+</script>
+
 <div class="register-page">
     <div class="form">
         <h1>Register</h1>
       <form class="register-form">
-        <input type="text" placeholder="First Name" class="input-form" name="firstname" id="firstname"/>
-        <input type="text" placeholder="Last Name" class="input-form" name="lastname" id="lastname"/>
-        <input type="text" placeholder="Username" class="input-form" name="username" id="username"/>
-        <input type="email" placeholder="Email Address" class="input-form" />
-        <input type="password" placeholder="Password" class="input-form" name="password" id="password"/>
-        <button type="submit" class="formSubmit">create</button>
+        <input bind:value={firstName} type="text" placeholder="First Name" class="input-form" name="firstname" id="firstname" required/>
+        <input bind:value={lastName} type="text" placeholder="Last Name" class="input-form" name="lastname" id="lastname" required/>
+        <input bind:value={username} type="text" placeholder="Username" class="input-form" name="username" id="username" required/>
+        <input bind:value={email} type="email" placeholder="Email Address" class="input-form" required/>
+        <input bind:value={password} type="password" placeholder="Password" class="input-form" name="password" id="password" required/>
+        <input bind:value={confirmPassword} type="password" placeholder="Confirm Password" class="input-form" name="confirmpassword" id="confirmpassword" required/>
+        <button on:click={createUser} class="formSubmit">create</button>
       </form>
     </div>
   </div>
