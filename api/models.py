186a5d50c7c4745819fd986ebeeec05400b522f7
SWarrior97 <carlosgomes1997.edu@gmail.com>
2020-10-11 12:22:47 +0100

    relationship created

--- api/models.py
+++ api/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 class Spell(models.Model):
     name = models.CharField(max_length=200)
-    damage = models.PositiveIntegerField
-    image = models.ImageField
-    color = models.CharField(max_length=200)
+    damage = models.PositiveIntegerField(default = 0)
+    image = models.ImageField(upload_to ='uploads/',default = '')
 
 class Enemy(models.Model):
     name = models.CharField(max_length=200)
-    life = models.PositiveIntegerField
-    image = models.ImageField(upload_to ='uploads/')
+    life = models.PositiveIntegerField(default = 0)
+    image = models.ImageField(upload_to ='uploads/',default = '')
     
     
 class Color(models.Model):
     name = models.CharField(max_length=200)
     hexa = models.CharField(max_length=200)
+    spells = models.ManyToManyField(Spell)
 
 def __str__(self):
     return self.name
