095d208853b8f824aaadda7a11c0c6c525a35f3f
SWarrior97 <carlosgomes1997.edu@gmail.com>
2020-10-12 13:48:24 +0100

    change color/spell relationship

--- color/models.py
+++ color/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from spell.models import Spell
+
 
 class Color(models.Model):
     name = models.CharField(max_length=200)
     hexa = models.CharField(max_length=200)
-    spells = models.ManyToManyField(Spell)
+    
 
 def __str__(self):
     return self.name
--- spell/models.py
+++ spell/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from color.models import Color
 
 # Create your models here.
 class Spell(models.Model):
     name = models.CharField(max_length=200)
     damage = models.PositiveIntegerField(default = 0)
     image = models.ImageField(upload_to ='uploads/',default = '')
+    colors = models.ManyToManyField(Color)
 
 def __str__(self):
     return self.name
