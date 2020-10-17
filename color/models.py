dabc41c4cb481fb034221152acaa31ba29c9316d
xQsme <pedro-ferreira92@hotmail.com>
2020-10-17 21:28:31 +0100

    auth progress?

--- color/models.py
+++ color/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-
 class Color(models.Model):
     name = models.CharField(max_length=200)
     hexa = models.CharField(max_length=200)
-    
 
 def __str__(self):
     return self.name
